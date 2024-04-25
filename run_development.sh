eb7682a2a377e6c46786210217f0e2a8c267d821
Luan Tran <8613937+luantr@users.noreply.github.com>
2024-04-25 23:36:09 +0200

    add separate pdf for ssps digital and reserved user

--- run_development.sh
+++ run_development.sh
@@ -1,15 +1,18 @@
 #!/bin/bash
 
 CURRENT_PATH="$(cd "$(dirname "$1")"; pwd)/$(basename "$1")"
- 
+
+docker pull ghcr.io/navikt/pdfgen:2.0.30
 docker run \
         -v "$CURRENT_PATH/templates:/app/templates" \
         -v "$CURRENT_PATH/fonts:/app/fonts" \
         -v "$CURRENT_PATH/data:/app/data" \
         -v "$CURRENT_PATH/resources:/app/resources" \
         -p 8080:8080 \
+        -e DEV_MODE=true \
         -e DISABLE_PDF_GET=false \
+        -e JDK_JAVA_OPTIONS \
         -it \
         --rm \
-        syfopdfgen:latest
+        ghcr.io/navikt/pdfgen:2.0.30
 
